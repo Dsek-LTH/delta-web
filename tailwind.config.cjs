f556f1d19764837041720fba61d2191dd099a278
sennahhh <hannes@bolmehag.se>
2026-06-26 13:25:03 +0200

    removed unneeded animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,7 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      keyframes: {
-        blink: {
-          "0%, 100%": { opacity: "1" },
-          "50%": { opacity: "0" },
-        },
-      },
-      animation: {
-        // step-end creates the sharp on/off terminal effect
-        blink: "blink 1s step-end infinite",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
