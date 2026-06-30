6b134e72a208ab6c796cda9e728f39383b80b230
Rufus Kogg Röjder <rufus@rojder.se>
2026-06-30 16:27:04 +0200

    feat: add internationalization
    * Fix the indentation
    
    * Add i18n support to config
    
    * Add English fallback for Swedish
    
    * I18n go brr
    
    * comment empty file
    
    * Translate page
    
    * Style markdown porperly
    
    Co-authored-by: Christofer Gärtner <crillegartner@gmail.com>
    Refs: #25

---
+++ src/i18n/ui.ts
@@ -0,0 +1,29 @@
+export const languages = {
+  sv: "Svenska",
+  en: "English",
+};
+
+export const defaultLang = "sv";
+
+export const ui = {
+  en: {
+    "tile.when": "When",
+    "tile.where": "Where",
+    "tile.what": "What",
+    "tile.when.networking": "Networking event after",
+    "button.attend": "Attend",
+    "footer.contact": "Contact us",
+    "footer.development":
+      "This page is still in development, the source code is available on",
+  },
+  sv: {
+    "tile.when": "När",
+    "tile.where": "Var",
+    "tile.what": "Vad",
+    "tile.when.networking": "Nätverkningsevent efter",
+    "button.attend": "Delta på",
+    "footer.contact": "Kontakta oss",
+    "footer.development":
+      "Denna sida är fortfarande under utveckling, källkoden finns tillgänglig på",
+  },
+} as const;
