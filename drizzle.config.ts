6237be4f257cb7803e549a0a5bb249ae3d319205
Rufus Kogg Röjder <rufus@rojder.se>
2026-07-03 17:29:16 +0200

    move migrations to src/db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   schema: "./src/db/schema.ts",
-  out: "./drizzle/migrations",
+  out: "./src/db/migrations",
   dialect: "sqlite",
   dbCredentials: {
     url: process.env.DB_FILE_NAME!,
