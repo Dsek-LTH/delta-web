de6c82efd87d7652f5987a30f14cb1b0c6a63d1a
Rufus Kogg Röjder <rufus@rojder.se>
2026-07-27 17:44:43 +0200

    Skip auth check on static content.

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,11 @@ import { auth } from "@/auth";
 import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware(async (context, next) => {
+  const pathname = new URL(context.request.url).pathname;
+  if (pathname.startsWith("/api/auth") || pathname.match(/\.\w+$/)) {
+    return next();
+  }
+
   const isAuthed = await auth.api.getSession({
     headers: context.request.headers,
   });
